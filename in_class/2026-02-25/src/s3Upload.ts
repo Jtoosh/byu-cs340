937f70a8a52f09e77bd222a508b6f6fd6ef1e776
James Teuscher <james.teuscher@outlook.com>
2026-02-25 16:11:29 -0700

    Update (in_class) finished s3 exercise

---
+++ in_class/2026-02-25/src/s3Upload.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+
+import {
+    S3Client,
+    PutObjectCommand,
+} from "@aws-sdk/client-s3";
+
+s3upload();
+
+async function s3upload() {
+    if (process.argv.length !== 3) {
+        console.log("Specify the file to upload on the command line");
+        process.exit();
+    }
+
+    try {
+        const client = new S3Client();
+        const fileContent = fs.readFileSync(process.argv[2]!);
+
+        const params = {
+            "Body": fileContent,
+            "Bucket": "340-test",
+            "Key": process.argv[2]! 
+        }
+
+        const command = new PutObjectCommand(params)
+        const response = await client.send(command)
+
+        console.log("File upload successful with ", response.$metadata.httpStatusCode);
+    } catch (error) {
+        console.log(error);
+    }
+}
