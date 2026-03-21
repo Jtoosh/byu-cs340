b8612b1dd03730b65aef9f01ba7d9656948a370b
James Teuscher <james.teuscher@outlook.com>
2026-03-20 22:04:28 -0600

    Revert "Update (tweeter server) M3 progress, started parameterizing main.tf"
    This reverts commit d019a8d550ba21514e1e18789d518daf37dd8f58.

--- tweeter-web-starter/tweeter-server/main.tf
+++ tweeter-web-starter/tweeter-server/main.tf
@@ -21,6 +21,32 @@ provider "aws" {
   region = "us-west-2"
 }
 
+locals {
+  tweeter_api_id      = aws_api_gateway_rest_api.TweeterAPI.id
+  getfollowees_id     = aws_api_gateway_resource.GetFollowees.id
+  getfollowees_method = aws_api_gateway_method.loadMoreFollowees.http_method
+
+  cors_method_response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+
+  cors_integration_response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
+  }
+
+  error_responses = {
+    "400" = {
+      selection_pattern = ".*\\[400\\].*"
+    }
+    "500" = {
+      selection_pattern = ".*\\[500\\].*"
+    }
+  }
+}
 
 # Look up existing API Gateway REST API by name for import purposes
 data "aws_api_gateway_rest_api" "existing_api" {
@@ -51,7 +77,38 @@ resource "aws_iam_role" "backend_lambda_handler" {
   assume_role_policy = data.aws_iam_policy_document.role.json
 }
 
+# Package the Lambda function code
+data "archive_file" "lambda_package" {
+  type        = "zip"
+  source_dir  = "${path.module}/dist/"
+  output_path = "${path.module}/package/backend.zip"
+}
 
+resource "archive_file" "layer_package" {
+  type        = "zip"
+  source_dir  = "${path.module}/layer"
+  output_path = "${path.module}/package/dependencies/layer.zip"
+}
+
+# Lambda Resources
+
+resource "aws_lambda_layer_version" "dependencies" {
+  depends_on       = [archive_file.layer_package]
+  filename         = archive_file.layer_package.output_path
+  layer_name       = "tweeter_server_layer"
+  source_code_hash = archive_file.layer_package.output_base64sha256
+}
+
+resource "aws_lambda_function" "GetFolloweesLambda" {
+  depends_on       = [data.archive_file.lambda_package]
+  function_name    = "GetFolloweesLambda"
+  role             = aws_iam_role.backend_lambda_handler.arn
+  filename         = data.archive_file.lambda_package.output_path
+  runtime          = "nodejs20.x"
+  handler          = "lambda/GetFolloweesLambda.handler"
+  layers           = [aws_lambda_layer_version.dependencies.arn]
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+}
 
 # API Gateway resources
 
@@ -62,11 +119,12 @@ resource "aws_api_gateway_rest_api" "TweeterAPI" {
   }
 }
 
+
 resource "aws_api_gateway_resource" "Resources" {
   for_each = var.api_resource
   rest_api_id = aws_api_gateway_rest_api.TweeterAPI.id
   parent_id   = aws_api_gateway_rest_api.TweeterAPI.root_resource_id
-  path_part   = each.value.pathPart
+  path_part   = "getfollowees"
 }
 
 module "api_gateway_endpoint_setup" {
